b568d24f2c8e9e0b9c8584a7b52ca3ca60d1f35e
Victor Coelho <42388296+victoracoelho@users.noreply.github.com>
2020-03-18 01:32:19 -0300

    Update fscore.py

--- fscore.py
+++ fscore.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+
 ### FUNÇÃO PARA TRANSFORMAR O ROA DE STRING PARA FLOAT
 def roa(df):
     df = df[6][1][3].replace('%', '')
@@ -15,6 +16,7 @@ def fco(df2):
     fco = fco.replace('.', '')
     return float(fco)
 
+
 ### FUNÇÃO PARA TRANSFORMAR AS VARIÁVEIS PARA FLOAT E FAZER O CÁLCULO DA VARIAÇÃO DO ROA
 def roa_var(df3, df4):
     dre = df3[1]
@@ -26,6 +28,30 @@ def roa_var(df3, df4):
     roa_var = (dre1/bp1) - (dre2/bp2)
     return roa_var
 
+################## CONSERTAR FUNÇÕES ABAIXO ##################
+def lucroliq(df):
+    lucro = df[3][1][5].replace('R$', '')
+    lucro = lucro.replace('B', '')
+    lucro = lucro.replace(',', '.')
+    lucro = lucro.replace('M', '')
+    if '-' in lucro:
+        lucro = lucro.replace('-', '')
+        float(lucro)
+        lucro = 0 - lucro
+        return lucro
+    else:
+        return float(lucro)
+
+
+def alavancagem(df4, df5):
+    divida = df5[1]
+    ativos = df4[1]
+    divida1 = float(divida['31/12/2019 (R$ mil)'][47].replace('.', ''))
+    divida2 = float(divida['31/12/2018 (R$ mil)'][47].replace('.', ''))
+    ativos1 = float(ativos['31/12/2019 (R$ mil)'][1].replace('.', ''))
+    ativos2 = float(ativos['31/12/2018 (R$ mil)'][1].replace('.', ''))
+    alav = (ativos1 / divida1) - (ativos2 / divida2)
+    return alav
 ################################################### PROGRAMA PRINCIPAL ###################################################
 
 fscore = 0
@@ -44,6 +70,9 @@ df3 = pd.read_html(link3)
 link4 = 'https://www.investsite.com.br/balanco_patrimonial_ativo.php?cod_negociacao=' + tick
 df4 = pd.read_html(link4)
  
+link5 = 'https://www.investsite.com.br/balanco_patrimonial_passivo.php?cod_negociacao=' + tick
+df5 = pd.read_html(link5)
+
 if roa(df) > 0:
     fscore += 1
 
@@ -53,5 +82,10 @@ if fco(df2) > 0:
 if roa_var(df3, df4) > 0:
     fscore += 1
 
+if (fco(df2) / 1000000) > lucroliq(df):
+    fscore += 1
+
+
 print(f'F-Score de Piotroski para o ativo {tick} é igual a {fscore}')
 
+print(alavancagem(df4, df5))
